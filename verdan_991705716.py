617e571633be2551d17238f2be185d3222082b35
KYRK915 <85113824+KYRK915@users.noreply.github.com>
2023-02-25 19:42:34 -0500

    Add files via upload

--- verdan_991705716.py
+++ verdan_991705716.py
@@ -10,13 +10,57 @@ A program that calculates the average CO2 levels in different buildings. It take
 def average():
 
     y = int(input("Enter the number of days for the readings: "))
-    print (y)
+    aveList = []
 
     #This asks the user for the CO2 readings based on how many days they input
     while y > 0:
         time.sleep(0.5)
         z = int(input("Enter the C02 for Day {} (Between 20 - 50 PPM) : ". format(y)))
-        print (z)
+
+        # This makes sure the input is between 20 and 50
+        if z >= 20 and z <= 50:
+            aveList.append(z)
+        elif z <20 or z >50:
+            while z <20 or z >50:
+                print ('Wrong Input')
+                z = int(input("Enter the C02 for Day {} (Between 20 - 50 PPM) : ". format(y)))
+                if z >= 20 and z <= 50:
+                    aveList.append(z)
+       
         y-= 1 
 
-average()
+    time.sleep(0.5)     
+    print("Rounded Average Readings", sum(aveList)/ len(aveList), 'PPM')
+
+
+
+# this function generates the random sensors
+def sensorDeploy ():
+
+    x = int(input("Enter the number of sensors deployed across Sheridan Campus: "))
+
+    while x > 0:
+        time.sleep(0.5)
+        print("This is for Sensor", x, " at position", '(' + str(random.randint(1,100)) + ',' + str(random.randint(1,100)) + ')' )            
+        x -= 1
+        average()
+        time.sleep(0.5)
+
+
+    continueProgram()
+
+
+# this function determines if the user still wants to continue or not
+def continueProgram ():
+    a = input("Do you want to continue: (Y)es or (N)o: ").upper()
+    print(a)
+
+    if a == 'YES' or a == 'Y':
+        time.sleep(0.5)
+        sensorDeploy()
+    
+    elif a == 'NO' or a == 'N':
+        time.sleep(0.5)
+        print ('Exiting Program')
+
+sensorDeploy()
